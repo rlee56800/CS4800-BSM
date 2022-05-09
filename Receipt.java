85cccf448f2d8c9837884c22db7ae6fea8c0d46b
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2022-05-09 02:23:19 -0700

    recipt thoughts

--- Receipt.java
+++ Receipt.java
@@ -9,6 +9,23 @@ public class Receipt {
         this.period = period;
     }
 
+    public String printReceipt() {
+        return "a";
+        // do we want this
+        // print:
+        // product.getID()/whatever
+        // product.getName()
+        // product.getQuantity()
+
+        // product.getPrice() * product.getQuantity
+        // or print each individually?
+        // for(int i = quantity, etc)
+        // that stuff
+
+        // for(int i = quantity)
+        // all of that
+    }
+
     public double getTotal() {
         return order.calculateTotal();
     }
